4d711726b3f48acc47f25846aeadf2238bf8331e
szmisztal <sz.misztal@gmail.com>
2023-12-25 12:12:35 +0100

    create entry model

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,2 +1,8 @@
+from django.utils import timezone
 from django.db import models
 
+
+class Entry(models.Model):
+    title = models.CharField(max_length = 50, blank = False)
+    publication_date = models.DateField(default = timezone.now)
+    text = models.TextField(max_length = 1000, blank = False)
