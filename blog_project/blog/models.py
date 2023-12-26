5dd09ba37f6bcbfff54e363ffabd19da584b886e
szmisztal <sz.misztal@gmail.com>
2023-12-26 10:45:27 +0100

    add author field to entry model

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import models
 
@@ -6,3 +7,4 @@ class Entry(models.Model):
     title = models.CharField(max_length = 50, blank = False)
     publication_date = models.DateField(default = timezone.now)
     text = models.TextField(max_length = 1000, blank = False)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
