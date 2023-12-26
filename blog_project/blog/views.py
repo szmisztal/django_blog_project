7bc1fca8b11893d5586a65b7002639416fb25d42
szmisztal <sz.misztal@gmail.com>
2023-12-26 10:46:18 +0100

    add views for all CRUD operations

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,13 +1,82 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import generic
-from .forms import RegisterForm, LoginForm
 from django.contrib import messages
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .forms import RegisterForm, LoginForm, EntryForm
+from .models import Entry
 
+
+# ENTRIES VIEWS:
 
 class HomepageView(generic.TemplateView):
     template_name = "homepage.html"
 
+@method_decorator(login_required, name = "dispatch")
+class EntryDetailView(DetailView):
+    model = Entry
+    context_object_name = "entry_detail"
+    template_name = "entry_detail.html"
+
+@method_decorator(login_required, name = "dispatch")
+class EntryListView(ListView):
+    model = Entry
+    paginate_by = 25
+    context_object_name = "entries_list"
+    template_name = "entries_list.html"
+
+    def get_queryset(self):
+        return Entry.objects.order_by("author", "publication_date")
+
+@method_decorator(login_required, name = "dispatch")
+class EntryCreateView(LoginRequiredMixin, CreateView):
+    model = Entry
+    form_class = EntryForm
+    template_name = "entry_form.html"
+    success_url = "/blog/entries_list/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        messages.success(self.request, "Entry was created successfully")
+        return response
+
+@method_decorator(login_required, name = "dispatch")
+class EntryUpdateView(UpdateView):
+    model = Entry
+    form_class = EntryForm
+    template_name = "entry_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy("entry_detail", kwargs = {"pk": self.object.pk})
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Entry was edited successfully")
+        return response
+
+@method_decorator(login_required, name = "dispatch")
+class EntryDeleteView(DeleteView):
+    model = Entry
+    template_name = "entry_delete.html"
+    success_url = "/blog/entries_list/"
+
+    def get_success_url(self):
+        return reverse_lazy("entries_list")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Entry was deleted successfully")
+        return response
+
+
+# USER SERVICE FUNCTIONS:
 
 def sign_up(request):
     template_name = "register.html"
@@ -20,7 +89,6 @@ def sign_up(request):
             user = form.save(commit = False)
             user.username = user.username.lower()
             user.save()
-            user.set_group()
             messages.success(request, "Registration success, you can log in now.")
             return redirect("login")
         else:
