3d2481762096b3d73cdc242b503e8aa41d4820e2
szmisztal <sz.misztal@gmail.com>
2023-12-26 10:45:46 +0100

    add urls for all CRUD operations

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("register/", views.sign_up, name = "register"),
     path("login/", views.sign_in, name = "login"),
     path("logout/", views.sign_out, name = "logout"),
+    path("entry/<int:pk>/", views.EntryDetailView.as_view(), name = "entry_detail"),
+    path("entries_list/", views.EntryListView.as_view(), name = "entries_list"),
+    path("entry_create/", views.EntryCreateView.as_view(), name = "entry_create"),
+    path("entry_update/<int:pk>", views.EntryUpdateView.as_view(), name = "entry_update"),
+    path("entry_delete/<int:pk>/", views.EntryDeleteView.as_view(), name = "entry_delete")
 ]
